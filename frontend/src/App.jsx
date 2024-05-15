599be4e550cb76663885e21ef514686826e942e7
prth8564 <preetham10107@gmail.com>
2024-05-15 08:53:51 +0530

    deploy-1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Home.jsx";
+import Login from "./Login.jsx";
+import SignUp from "./SignUp.jsx";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
+
+function App() {
+	const { authUser } = useAuthContext();
+	return (
+		<div className='p-4 h-screen flex items-center justify-center'>
+			<Routes>
+				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
+				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
+			</Routes>
+			<Toaster />
+		</div>
+	);
+}
+
+export default App;
