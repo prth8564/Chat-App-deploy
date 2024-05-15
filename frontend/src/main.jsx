a248b8a684e376467bdb9e1ea37e0863565ba81b
prth8564 <preetham10107@gmail.com>
2024-05-15 09:01:27 +0530

    little changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom';
-import { AuthContextProvider } from '.AuthContext.jsx'
+import { AuthContextProvider } from './AuthContext.jsx'
 import { SocketContextProvider } from './SocketContext.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   
